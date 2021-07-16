2be7cc59f119735981dd3790c0ed31f73dd94c5d
bitaraf.eng <en.bitaraf@gmail.com>
2021-07-17 00:27:53 +0430

    test commit

--- StartPointCSharpDotNetConsole/StartPointCSharpDotNetConsole/Program.cs
+++ StartPointCSharpDotNetConsole/StartPointCSharpDotNetConsole/Program.cs
@@ -7,7 +7,7 @@ namespace StartPointCSharpDotNetConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("In the name of god ");
+            
         }
     }
 }
